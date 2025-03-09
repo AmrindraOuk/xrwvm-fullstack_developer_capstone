905e657400e316a73592d17b77ec914c83cf4cc0
AmrindraOuk <oamrindraouk@gmail.com>
2025-03-09 14:09:04 -0400

    Fixed error

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -15,42 +15,42 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-# class CarModel(models.Model):
-#     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-#     name = models.CharField(max_length=100)
-#     CAR_TYPES = [
-#         ('SEDAN', 'Sedan'),
-#         ('SUV', 'SUV'),
-#         ('WAGON', 'Wagon'),
-#         # Add more choices as required
-#     ]
-#     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-#     year = models.IntegerField(default=2023,
-#         validators=[
-#             MaxValueValidator(2023),
-#             MinValueValidator(2015)
-#         ])
-#     # Other fields as needed
-
-#     def __str__(self):
-#         return self.name  # Return the name as the string representation
-
-        
 class CarModel(models.Model):
-    # make = models.ForeignKey(CarMake, null=True, on_delete = models.CASCADE)
-    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
-    dealer_id = models.IntegerField()
-    name = models.CharField(max_length=255)
-    SEDAN = "Sedan"
-    SUV = "SUV"
-    WAGON = "WAGON"
-    TYPE_CHOICES = (
-        (SEDAN, "Sedan"),
-        (SUV, "SUV"),
-        (WAGON, "Wagon"),
-    )
-    model_type  = models.CharField(max_length=10, choices=TYPE_CHOICES)
-    year = models.DateField()
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+    # Other fields as needed
 
     def __str__(self):
-        return f"{self.car_make} {self.name} ({self.year})"
+        return self.name  # Return the name as the string representation
+
+        
+# class CarModel(models.Model):
+#     # make = models.ForeignKey(CarMake, null=True, on_delete = models.CASCADE)
+#     car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+#     dealer_id = models.IntegerField()
+#     name = models.CharField(max_length=255)
+#     SEDAN = "Sedan"
+#     SUV = "SUV"
+#     WAGON = "WAGON"
+#     TYPE_CHOICES = (
+#         (SEDAN, "Sedan"),
+#         (SUV, "SUV"),
+#         (WAGON, "Wagon"),
+#     )
+#     model_type  = models.CharField(max_length=10, choices=TYPE_CHOICES)
+#     year = models.DateField()
+
+#     def __str__(self):
+#         return f"{self.car_make} {self.name} ({self.year})"
